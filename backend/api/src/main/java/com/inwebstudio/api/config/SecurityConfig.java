3d9c080a276c5f1c4839b3847d447cc04e452be0
IngridNogueira <isnogueira601@gmail.com>
2026-07-22 18:06:47 -0300

    feat: add permission control to create users

--- backend/api/src/main/java/com/inwebstudio/api/config/SecurityConfig.java
+++ backend/api/src/main/java/com/inwebstudio/api/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -43,11 +44,12 @@ public class SecurityConfig {
                         UsernamePasswordAuthenticationFilter.class
                 )
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/users").hasRole("ADMIN")
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
-                                "/auth/login"
-                        ).permitAll()
+                                "/auth/login")
+                        .permitAll()
                         .anyRequest().authenticated()
                 )
                 .authenticationProvider(authenticationProvider());
